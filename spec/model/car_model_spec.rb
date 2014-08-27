559c87529b7c4f2de809289a62dc487377042e7e
Stephanie Chou <stephanie.chou@outlook.com>
2014-08-27 15:15:56 -0500

    car model spec skeleton

---
+++ spec/model/car_model_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe 'car' do
+
+  it 'should return a car object, class Car' do
+    pending
+ end
+
+ it 'should have a :make attribute' do
+  pending
+ end
+
+ it 'should have a :model attribute' do
+  pending
+ end
+
+ it 'should have a :year attribute' do
+  pending
+ end
+
+ it 'should convert kilometers to miles' do
+  pending
+ end
+
+ it 'should calulate fuel costs per day' do
+  pending
+ end
+
+
+end
