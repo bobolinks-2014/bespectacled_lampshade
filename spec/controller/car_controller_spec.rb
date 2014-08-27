b2197a3d84850eef2796ea52a67701282d11132e
Stephanie Chou <stephanie.chou@outlook.com>
2014-08-27 15:46:53 -0500

    car controller spec file added, test skeleton complete

---
+++ spec/controller/car_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "CarsController" do
+  it 'landing page loads' do
+    pending
+    #get :index
+    #expect(response.status).to eq 200
+  end
+end
---
+++ spec/view/car_view_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+feature 'initial view' do
+  context "on landing page" do
+    it "should see a text field for home address input" do
+      pending
+    end
+    it "should see a text field for destination address input" do
+      pending
+    end
+    it "should see a drop down for make of vehicle" do
+      pending
+    end
+    it "should see a drop down for model of vehicle disabled" do
+      pending
+    end
+    it "should see a drop down for year of vehicle disabled" do
+      pending
+    end
+    it "should see a drop down for year of vehicle enabled after make is selected" do
+      pending
+    end
+    it "should see a drop down for model of vehicle disabled after make is selected" do
+      pending
+    end
+    it "should see a drop down for year of vehicle enabled after model is selected" do
+      pending
+    end
+     it "can hit submit button after information has been obtained" do
+      pending
+    end
+  end
+end
+
+feature 'final view' do
+  context "after user submitted information on landing page" do
+    it "should see a table of information displaying cost information" do
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
