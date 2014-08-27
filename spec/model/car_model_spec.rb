2967c5f0e88f2ad01cc6573c77e09f1e2e81bb87
Stephanie Chou <stephanie.chou@outlook.com>
2014-08-27 17:11:44 -0500

    tests for car methods refactored

--- spec/model/car_model_spec.rb
+++ spec/model/car_model_spec.rb
@@ -17,27 +17,32 @@ describe 'car' do
   it 'should have a :year attribute' do
     expect(car).to respond_to(:year)
   end
-
-  it 'should convert kilometers to miles' do
-    expect(Car.kilometers_to_miles(10)).to eq(6.21)
-  end
-
-
-
-
 end
 
 describe "fuel_costs" do
   let(:car){Car.new}
+  let (:args){{gas_price: 2, distance:5.13, mpg:3}}
 
   it {should respond_to(:hash)}
 
   it 'should return a float' do
-    args = {gas_price: 2, distance:5.13, mpg:3}
     expect(car.fuel_costs(args)).to be_an_instance_of(Float)
   end
   it 'should return an answer with two decimal places' do
-    args = {gas_price: 2, distance:5.13, mpg:3}
     expect(car.fuel_costs(args)).to eq(car.fuel_costs(args).round(2))
+  end
+
+  it "should calculate fuel_costs" do
+    expect(car.fuel_costs(args)).to eq(3.42)
+  end
+end
+
+describe "kilometers_to_miles" do
+  let(:conversion){Car.kilometers_to_miles(10)}
+  it 'should return a float' do
+    expect(conversion).to be_an_instance_of(Float)
+  end
+  it 'should convert kilometers to miles' do
+    expect(conversion).to eq(6.21)
   end
 end
