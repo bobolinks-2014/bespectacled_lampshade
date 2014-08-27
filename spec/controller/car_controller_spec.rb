1f9d13f9f74ddfd5423106237801f84e1aa64c80
Stephanie Chou <stephanie.chou@outlook.com>
2014-08-27 16:07:05 -0500

    filled in models testing skeleton

--- spec/controller/car_controller_spec.rb
+++ spec/controller/car_controller_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 
 describe "CarsController" do
   it 'landing page loads' do
-    pending
-    #get :index
-    #expect(response.status).to eq 200
+    get :index
+    expect(response.status).to eq 200
   end
 end
--- spec/model/car_model_spec.rb
+++ spec/model/car_model_spec.rb
@@ -1,30 +1,35 @@
 require 'spec_helper'
 
 describe 'car' do
-
+  let(:car){Car.new}
   it 'should return a car object, class Car' do
-    pending
- end
+    expect(car).to be_a(Car)
+  end
 
- it 'should have a :make attribute' do
-  pending
- end
+  it 'should have a :make attribute' do
+    expect(car).to respond_to(:make)
+  end
 
- it 'should have a :model attribute' do
-  pending
- end
+  it 'should have a :model attribute' do
+    expect(car).to respond_to(:model)
+  end
 
- it 'should have a :year attribute' do
-  pending
- end
+  it 'should have a :year attribute' do
+    expect(car).to respond_to(:year)
+  end
 
- it 'should convert kilometers to miles' do
-  pending
- end
-
- it 'should calulate fuel costs per day' do
-  pending
- end
+  it 'should convert kilometers to miles' do
+    expect(Car.kilometers_to_miles(10)).to eq(6.21)
+  end
 
 
+
+
+end
+
+describe 'calculate fuel cost method' do
+  let(:car){Car.new}
+  it 'should calulate fuel costs per day' do
+    expect(Car).to respond_to(:fuel_costs).with(3).arguments
+  end
 end
