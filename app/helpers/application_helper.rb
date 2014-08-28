c035f1997f15289e028cf37b5f0f50a83b340a8d
Ryan Spencer <rjspencer82@gmail.com>
2014-08-28 13:06:21 -0500

    added method that calculate_distance and a test for the method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
+require 'net/http'
+require 'json'
+
 module ApplicationHelper
+
+  def calculate_distance(start_addr, destination_addr)
+    address1 = start_addr.gsub(" ", "+").gsub(",", "")
+    address2 = destination_addr.gsub(" ", "+").gsub(",", "")
+    uri = URI("http://maps.googleapis.com/maps/api/distancematrix/json?origins=#{address1}&destinations=#{address2}&mode=auto")
+    response_data = JSON.parse(Net::HTTP.get(uri))
+    kilometers = (response_data["rows"][0]["elements"][0]["distance"]['text']).gsub(" km", '').to_f
+    miles = ( kilometers * 0.621371 ).round(2)
+  end
+
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,10 +1,10 @@
 class Car < ActiveRecord::Base
 
-	def fuel_cost(args)
-		combined_mpg = (self.city_mpg.to_f * 0.45) + (self.highway_mpg.to_f * 0.55)
-		gallons_per_trip = args[:distance].to_i * 1/combined_mpg.to_f
-		cost_per_trip = ( args[:gas_price].to_f * gallons_per_trip ).round(2)
-	end
+  def fuel_cost(args)
+    combined_mpg = (self.city_mpg.to_f * 0.45) + (self.highway_mpg.to_f * 0.55)
+    gallons_per_trip = args[:distance].to_i * 1/combined_mpg.to_f
+    cost_per_trip = ( args[:gas_price].to_f * gallons_per_trip ).round(2)
+  end
 
   def self.kilometers_to_miles kilometers
     miles = ( kilometers * 0.621371 ).round(2)
