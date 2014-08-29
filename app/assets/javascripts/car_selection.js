d340d89baf7f5db0c6efcd6ac14a59592a4c4aab
Stephanie Chou <stephanie.chou@outlook.com>
2014-08-28 21:16:45 -0500

    card and cardview models. it works

--- app/assets/javascripts/car_selection.js
+++ app/assets/javascripts/car_selection.js
@@ -11,6 +11,10 @@ function Input(){
   this.year;
 }
 
+
+
+
+
 $('#make').change(function(e) {
 
   var selected = $(this).find(":selected").text();
@@ -51,8 +55,9 @@ $('#user').submit(function(e){
   var request = $.get('/submit', input);
   request.done(function(data){
     console.log(data);
-
-    $('.cards').prepend('<div class="large-3 columns "><ul class="pricing-table"><li class="title">'+data.name+'</li><li><img  src="http://media.caranddriver.com/images/media/51/dissected-lotus-based-infiniti-emerg-e-sports-car-concept-top-image-photo-451994-s-original.jpg" height="65" width="136" /></li><li class="description">'+data.city_mpg +'MPG City /'+data.hwy_mpg+'MPG Hwy</li><li class="bullet-item">$'+data.monthly_cost+'mo</li><li class="bullet-item">$'+data.monthly_cost+'yr</li><li class="cta-button"><a href="#" class="button round tiny">X</a></li></ul></div>');
-
+    card = new Card(data);
+    cardView = new CardView(card)
+    cardView.displayCard();
+    // $('.cards').append('<div class="large-3 columns "><ul class="pricing-table"><li class="title">'+data.name+'</li><li><img  src="http://media.caranddriver.com/images/media/51/dissected-lotus-based-infiniti-emerg-e-sports-car-concept-top-image-photo-451994-s-original.jpg" height="65" width="136" /></li><li class="description">'+data.city_mpg +'MPG City /'+data.hwy_mpg+'MPG Hwy</li><li class="bullet-item">$'+data.monthly_cost+'mo</li><li class="bullet-item">$'+data.yearly_cost+'yr</li><li class="cta-button"><a href="#" class="button round tiny">X</a></li></ul></div>');
   });
 });
