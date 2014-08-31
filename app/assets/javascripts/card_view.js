bcc7196572a17529c07038c8c885c350b0418d78
Ryan Spencer xubuntu-desktop <rjspencer82@gmail.com>
2014-08-31 14:26:45 -0500

    refactored edmunds api calls

--- app/assets/javascripts/card_view.js
+++ app/assets/javascripts/card_view.js
@@ -14,51 +14,55 @@ CardView.prototype.addCardPhoto = function() {
 	make = $('#make').find(":selected").text();
   model = $('#model').find(":selected").text();
   year = $('#year').find(":selected").text();
-  this.getEdmundsPhoto(make, model, year);
+  carId = this.getEdmundsData(make, model, year);
 }
 
-CardView.prototype.getEdmundsPhoto = function(make, model, year) {
+CardView.prototype.getEdmundsData = function(make, model, year) {
 	model = model.split(" ")[0].split("/")[0]; // Use only the first word
 	url = "http://api.edmunds.com/api/vehicle/v2/" + make + "/" + model + "?year=" + year + "&view=basic&fmt=json&api_key=xcs69p6sfvqx4dg3tupjg8hn";
 	var edmundsRequest = $.get(url);
-    edmundsRequest.done(function(data){
-      car_id = data["years"][0]["styles"][0]["id"];
-      currentYear = new Date().getFullYear()
-      if (year >= currentYear){
-        priceUrl = " http://api.edmunds.com/v1/api/tmv/tmvservice/calculatenewtmv?styleid=" + car_id + "&zip=60610&fmt=json&api_key=xcs69p6sfvqx4dg3tupjg8hn"
-      } else {
-        mileage = (currentYear - year) * 15000;
-        priceUrl = "http://api.edmunds.com/v1/api/tmv/tmvservice/calculateusedtmv?styleid=" + car_id + "&condition=clean&mileage=" + mileage + "&zip=60610&fmt=json&api_key=xcs69p6sfvqx4dg3tupjg8hn";
-      }
+  edmundsRequest.done(function(data){
+    carId = data["years"][0]["styles"][0]["id"];
+    this.edmundsPriceToCard(carId, year);
+    this.edmundsPhotoToCard(carId); 
+  }.bind(this));
+}
 
-      edmundsPriceRequest = $.get(priceUrl);
-      edmundsPriceRequest.done(function(priceData) {
-        if(priceData.tmv.nationalBasePrice.baseMSRP > 0) {
-          var retail = currencyString(priceData.tmv.nationalBasePrice.baseMSRP);
-        } else {
-          var retail = currencyString(priceData.tmv.nationalBasePrice.usedTmvRetail)
-        }
-        $(".purchase-price:first").append(retail);
+CardView.prototype.edmundsPriceToCard = function(carId, year) {
+  currentYear = new Date().getFullYear()
+  if (year >= currentYear){
+    priceUrl = " http://api.edmunds.com/v1/api/tmv/tmvservice/calculatenewtmv?styleid=" + carId + "&zip=60610&fmt=json&api_key=xcs69p6sfvqx4dg3tupjg8hn"
+  } else {
+    mileage = (currentYear - year) * 15000;
+    priceUrl = "http://api.edmunds.com/v1/api/tmv/tmvservice/calculateusedtmv?styleid=" + carId + "&condition=clean&mileage=" + mileage + "&zip=60610&fmt=json&api_key=xcs69p6sfvqx4dg3tupjg8hn";
+  }
+  edmundsPriceRequest = $.get(priceUrl);
+  edmundsPriceRequest.done(function(priceData) {
+    if(priceData.tmv.nationalBasePrice.baseMSRP > 0) {
+      var retail = currencyString(priceData.tmv.nationalBasePrice.baseMSRP);
+    } else {
+      var retail = currencyString(priceData.tmv.nationalBasePrice.usedTmvRetail)
+    }
+    $(".purchase-price:first").append(retail);
+  });
+}
+
+CardView.prototype.edmundsPhotoToCard = function(carId) {
+  photo_list_url = "http://api.edmunds.com/v1/api/vehiclephoto/service/findphotosbystyleid?styleId=" + carId + "&fmt=json&api_key=xcs69p6sfvqx4dg3tupjg8hn";
+  edmundsPhotoRequest = $.get(photo_list_url);
+  edmundsPhotoRequest.done(function(list) {
+    photoTypes = ["S", "F", "FQ", "RQ", "R", "O", "PROFILE", "D", "I"]
+    for( i=0; i < photoTypes.length; i++) {
+      photoList = list.filter(function(x){
+        if (x.shotTypeAbbreviation == photoTypes[i]) {return x; }
       });
-
-    	photo_list_url = "http://api.edmunds.com/v1/api/vehiclephoto/service/findphotosbystyleid?styleId=" + car_id + "&fmt=json&api_key=xcs69p6sfvqx4dg3tupjg8hn";
-      console.log(photo_list_url);
-    	edmundsPhotoRequest = $.get(photo_list_url);
-    	edmundsPhotoRequest.done(function(list) {
-    		photoTypes = ["S", "F", "FQ", "RQ", "R", "O", "PROFILE", "D", "I"]
-    		for( i=0; i < photoTypes.length; i++) {
-		    	photoList = list.filter(function(x){
-		    		if (x.shotTypeAbbreviation == photoTypes[i]) {return x; }
-		    	});
-		    	if(typeof photoList[0] !== 'undefined') {
-		    		if(typeof photoList[0].photoSrcs !== 'undefined') { break }
-		    	}
-	    	}
-	    	img_html = '<img src="http://media.ed.edmunds-media.com' + photoList[0].photoSrcs[0] + '" />';
-   		 	$(".car_thumb:first").html(img_html);
-   		});
-    });
-
+      if(typeof photoList[0] !== 'undefined') {
+        if(typeof photoList[0].photoSrcs !== 'undefined') { break }
+      }
+    }
+    img_html = '<img src="http://media.ed.edmunds-media.com' + photoList[0].photoSrcs[0] + '" />';
+    $(".car_thumb:first").html(img_html);
+  });
 }
 
 CardView.prototype.displayCard = function(){
