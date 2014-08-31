5a38e1be55ad1c5120d4326d41f6822f2bd5c228
Stephanie Chou <stephanie.chou@outlook.com>
2014-08-30 20:19:42 -0500

    clean up code

--- app/assets/javascripts/card_view.js
+++ app/assets/javascripts/card_view.js
@@ -3,7 +3,6 @@ function CardView(model){
   this.$el = $("<div></div>");
   this.$el.on("click", ".button", this.deleteCard);
   this.$el.on("click", this.drag.bind(this));
-
 }
 CardView.prototype.render = function(){
   this.$el.html('<div class="large-3 columns "><ul class="pricing-table draggable"><li class="title">'+this.model.name+'</li><li><img  src="http://media.caranddriver.com/images/media/51/dissected-lotus-based-infiniti-emerg-e-sports-car-concept-top-image-photo-451994-s-original.jpg" height="65" width="136" /></li><li class="description">'+this.model.city_mpg +'MPG City /'+this.model.hwy_mpg+'MPG Hwy</li><li class="bullet-item">$'+this.model.monthly_cost+'mo</li><li class="bullet-item">$'+this.model.yearly_cost+'yr</li><li class="cta-button"><a href="#" class="button round tiny">X</a></li></ul></div>');
@@ -18,8 +17,8 @@ CardView.prototype.deleteCard = function(event){
   event.preventDefault();
   $(this).parent().parent().parent().empty();
 }
-
-
 CardView.prototype.drag = function(event){
   this.$el.draggable();
 }
+
+
