31fba471e143480c21b391b373d218530f018fdd
Ryan Spencer xubuntu-desktop <rjspencer82@gmail.com>
2014-08-31 14:08:21 -0500

    photo and price from edmunds api to card working

--- app/assets/javascripts/card_view.js
+++ app/assets/javascripts/card_view.js
@@ -5,7 +5,7 @@ function CardView(model){
   this.$el.on("click", this.drag.bind(this));
 }
 CardView.prototype.render = function(){
-  this.$el.html('<div class="large-3 columns "><ul class="pricing-table"><li class="title">'+this.model.name+'</li><li class="car_thumb text-center"> </li><li class="description">'+this.model.city_mpg +'MPG City /'+this.model.hwy_mpg+'MPG Hwy</li><li class="bullet-item">$'+this.model.monthly_cost+'mo</li><li class="bullet-item">$'+this.model.yearly_cost+'yr</li><li class="bullet-item purchase-price">$</li><li class="cta-button"><a href="#" class="button round tiny">X</a></li></ul></div>');
+  this.$el.html('<div class="large-3 columns "><ul class="pricing-table"><li class="title">'+this.model.name+'</li><li class="car_thumb text-center"> </li><li class="description">'+this.model.city_mpg +'MPG City /'+this.model.hwy_mpg+'MPG Hwy</li><li class="bullet-item">$'+this.model.monthly_cost+'mo</li><li class="bullet-item">$'+this.model.yearly_cost+'yr</li><li class="bullet-item purchase-price"><h6>Estimated Retail</h6></li><li class="cta-button"><a href="#" class="button round tiny">X</a></li></ul></div>');
   this.addCardPhoto();
   return this.$el;
 }
@@ -20,10 +20,8 @@ CardView.prototype.addCardPhoto = function() {
 CardView.prototype.getEdmundsPhoto = function(make, model, year) {
 	model = model.split(" ")[0].split("/")[0]; // Use only the first word
 	url = "http://api.edmunds.com/api/vehicle/v2/" + make + "/" + model + "?year=" + year + "&view=basic&fmt=json&api_key=xcs69p6sfvqx4dg3tupjg8hn";
-	console.log(url);
 	var edmundsRequest = $.get(url);
     edmundsRequest.done(function(data){
-    	console.log(data);
       car_id = data["years"][0]["styles"][0]["id"];
       currentYear = new Date().getFullYear()
       if (year >= currentYear){
@@ -32,17 +30,15 @@ CardView.prototype.getEdmundsPhoto = function(make, model, year) {
         mileage = (currentYear - year) * 15000;
         priceUrl = "http://api.edmunds.com/v1/api/tmv/tmvservice/calculateusedtmv?styleid=" + car_id + "&condition=clean&mileage=" + mileage + "&zip=60610&fmt=json&api_key=xcs69p6sfvqx4dg3tupjg8hn";
       }
-      
-      console.log(priceUrl);
+
       edmundsPriceRequest = $.get(priceUrl);
       edmundsPriceRequest.done(function(priceData) {
-        console.log(priceData);
         if(priceData.tmv.nationalBasePrice.baseMSRP > 0) {
-          retail = priceData.tmv.nationalBasePrice.baseMSRP;
+          var retail = currencyString(priceData.tmv.nationalBasePrice.baseMSRP);
         } else {
-          retail = priceData.tmv.nationalBasePrice.usedTmvRetail
+          var retail = currencyString(priceData.tmv.nationalBasePrice.usedTmvRetail)
         }
-        $(".purchase-price:first").html("<h5>Est. Retail</h5>$" + String(retail).slice(0, -3) + "," + String(retail).slice(-3));
+        $(".purchase-price:first").append(retail);
       });
 
     	photo_list_url = "http://api.edmunds.com/v1/api/vehiclephoto/service/findphotosbystyleid?styleId=" + car_id + "&fmt=json&api_key=xcs69p6sfvqx4dg3tupjg8hn";
@@ -59,7 +55,6 @@ CardView.prototype.getEdmundsPhoto = function(make, model, year) {
 		    	}
 	    	}
 	    	img_html = '<img src="http://media.ed.edmunds-media.com' + photoList[0].photoSrcs[0] + '" />';
-	    	console.log(img_html);
    		 	$(".car_thumb:first").html(img_html);
    		});
     });
@@ -78,4 +73,6 @@ CardView.prototype.drag = function(event){
   this.$el.draggable();
 }
 
-
+function currencyString(number) {
+  return "$" + String(number).slice(0, -3) + "," + String(number).slice(-3);
+}
